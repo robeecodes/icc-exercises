ac8ab2b45699e8ec2bb5e90e9858c872927ec4d2
Robin <robin.code22@gmail.com>
2023-11-14 13:54:09 +0000

    feat(challenge-2-&&-challenge-3): complete challenge 2 and 3

--- week-7/challenge-2/sketch/sketch.js
+++ week-7/challenge-2/sketch/sketch.js
@@ -1,13 +1,26 @@
-let msg = "Hello";
-console.log("msg:", msg);
-console.log("msg: " + msg);
-console.log(`msg: ${msg}`);
+
+let x = -25;
 
 function setup() {
-  createCanvas(400, 400);
-  background(0);
+  console.log("setup() is running");
+  // put setup code here
+  createCanvas(400,600);
+  noFill();
+  stroke(255);
+  console.log(x);
+  x = x + 50;
+  console.log(x);
 }
 
 function draw() {
+  console.log("draw() is running");
+  // put drawing code here
+  background(100);
+  ellipse(x, height/2, 50);
 
+  x++;
+  if (x - 25 > width) {
+    x = 0;
+  }
+  //console.log(x);
 }
--- week-7/challenge-3/sketch/sketch.js
+++ week-7/challenge-3/sketch/sketch.js
@@ -1,13 +1,44 @@
-let msg = "Hello";
-console.log("msg:", msg);
-console.log("msg: " + msg);
-console.log(`msg: ${msg}`);
+const dotsArray = [];
 
 function setup() {
-  createCanvas(400, 400);
-  background(0);
+  createCanvas(710, 400);
+  noFill()
+  stroke(255,0,0);
+  // Create objects
+  for (let i = 0; i < 50; i++) {
+    // create a an object data type to store each dot values
+    let ellipseObj = {
+      x: random(width),
+      y: random(height),
+      diameter: random(10, 50),
+      speedX: random(-2, 2), // Random horizontal speed
+      speedY: random(-2, 2), // Random vertical speed
+    };
+    dotsArray.push(ellipseObj);
+  }
+  console.log(dotsArray);
 }
 
-function draw() {
 
+function draw() {
+  background(0);
+  // draw all the dots on each frame, moving there position
+  for (let i = 0; i < 50; i++) {
+    ellipseObj = dotsArray[i];
+
+    // Move the ellipse
+    ellipseObj.x += ellipseObj.speedX;
+    ellipseObj.y += ellipseObj.speedY;
+
+    // Check boundaries and bounce back
+    if (ellipseObj.x < 0 || ellipseObj.x > width) {
+      ellipseObj.speedX *= -1;
+    }
+    if (ellipseObj.y < 0 || ellipseObj.y > height) {
+      ellipseObj.speedY *= -1;
+    }
+
+    // Draw the ellipse
+    ellipse(ellipseObj.x, ellipseObj.y, ellipseObj.diameter);
+  }
 }
